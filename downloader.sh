f6b633cd5bb304c12e974c21a331e26986fb1269
J Chehrazi <105824619+Foxbox1000@users.noreply.github.com>
2023-05-22 12:01:16 -0800

    Update downloader.sh

--- downloader.sh
+++ downloader.sh
@@ -3,6 +3,6 @@
 count=1
 while [ $count -le 406 ]
 do
-  curl https://online.fliphtml5.com/lkcyu/adis/files/large/${number}.jpg
+  curl https://online.fliphtml5.com/lkcyu/adis/files/large/$count.jpg --output /home/kasm-user/Desktop/files/$count.jpg
   ((count++))
 done
